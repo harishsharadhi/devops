beaa5329c87bd468e7c8f2a439b16309b88dfc03
harishsharadhi <harishnryn@gmail.com>
2025-01-15 17:46:19 +0530

    thi is for main_2

---
+++ devops_branch.java
@@ -0,0 +1,16 @@
+public class devops_branch {
+
+
+
+
+     
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("My Second JA ctrl-Z puts the current running process on in the background and drops you back to the shell.1 May 2019 How to return to command line prompt after reading manual page");
+
+    }
+
+    
+}
