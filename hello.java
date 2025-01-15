b39b161bb4fa7019bc662932fb104189aead5153
harishsharadhi <harishnryn@gmail.com>
2025-01-15 17:18:08 +0530

    this is m fourth commit

--- hello.java
+++ hello.java
@@ -2,7 +2,14 @@ class hello
 {
     public static void main(String[] args)
     {
-        System.out.println("My First Java Program.Now i am persueing th course of Devops");
+        System.out.println("My First Java Program.Now i am persueing th course of Devops  A ctrl-Z puts the current running process on \"pause\" in the background and drops you back to the shell.1 May 2019\r\n" + //
+                        "\r\n" + //
+                        "How to return to command line prompt after reading manual page\r\n" + //
+                        "\r\n" + //
+                        "The FreeBSD Forums\r\n" + //
+                        "https://forums.freebsd.org › threads › how-to-return-to-c...\r\n" + //
+                        "How to start from command line?\r\n" + //
+                        "");
     }
 }
  
--- hello3.java
+++ hello3.java
@@ -3,6 +3,7 @@ class hello3
 {
     public static void main(String[] args)
     {
-        System.out.println("My Second Jav");
+        System.out.println("My Second JA ctrl-Z puts the current running process on in the background and drops you back to the shell.1 May 2019 How to return to command line prompt after reading manual page");
+
     }
 }
